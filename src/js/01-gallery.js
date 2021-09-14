ba780da8daace47c238bb4aa00e3d7c0e92a4cbc
Sebastian Hanusiak <drais234@gmail.com>
2021-09-14 12:22:20 +0200

    .js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
+import _default from '../../node_modules/simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesLibrary = {
+  itemsList: document.querySelector('.gallery'),
+};
+
+function makeGalleryItems(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a> `;
+    })
+    .join('');
+}
+
+imagesLibrary.itemsList.innerHTML = makeGalleryItems(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  showCounter: false,
+  disableScroll: false,
+  captionsData: 'alt',
+  captionDelay: 250,
+  loop: true,
+});
